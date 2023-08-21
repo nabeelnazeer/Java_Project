648a7c3ae81fbe155f0719c797ec59f9206cfe53
jeslinpjames <jeslinpjames007@gmail.com>
2023-08-21 11:01:08 +0530

    First Commit Package created

---
+++ Fitness_App/src/main/java/com/mycompany/fitness_app/Fitness_App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.fitness_app;
+
+/**
+ *
+ * @author jeslinpjames
+ */
+public class Fitness_App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
